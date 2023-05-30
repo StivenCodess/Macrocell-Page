d8475ea693a4eed958677d55141fb46e5d8fbf00
Tiven <vairtor@gmail.com>
2023-05-30 13:56:26 -0300

    🔧 change directory index.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
-export default {
-    root:"src",
-}
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    root: "src",
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'src/index.html'),
+          login: resolve(__dirname, 'src/pages/login.html'),
+        },
+      },
+    },
+  })
